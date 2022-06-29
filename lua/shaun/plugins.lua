2bd08676781395c14030e5d67b15a2ccb43b2d58
Shaun Barney <shaunbarney@outlook.com>
2022-06-29 08:51:25 +0100

    :zap: initial commit

---
+++ lua/shaun/plugins.lua
@@ -0,0 +1,141 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+-- Install your plugins here
+return packer.startup(function(use)
+	-- My plugins here
+	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("nvim-lua/plenary.nvim") -- Useful lua functions used by lots of plugins
+	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
+	use("numToStr/Comment.nvim") -- Easily comment stuff
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icon
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	})
+	use("akinsho/bufferline.nvim")
+	use("moll/vim-bbye")
+	use("akinsho/toggleterm.nvim")
+	use("tpope/vim-surround")
+	use({ "ellisonleao/glow.nvim", branch = "main" })
+
+	-- Latex
+	use("lervag/vimtex")
+
+	-- Colorschemes
+	use("lunarvim/colorschemes")
+	use("folke/tokyonight.nvim")
+	use("lunarvim/darkplus.nvim")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use("rcarriga/nvim-notify")
+	use("sunjon/shade.nvim")
+
+	-- cmp plugins
+	-- Remember to update sources in cmp
+	use("hrsh7th/nvim-cmp") -- The completion plugin
+	use("hrsh7th/cmp-buffer") -- buffer completions
+	use("hrsh7th/cmp-path") -- path completions
+	use("hrsh7th/cmp-cmdline") -- cmdline completions
+	use("saadparwaiz1/cmp_luasnip") -- snippet completions
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
+
+	-- snippets
+	use("L3MON4D3/LuaSnip") --snippet engine
+	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+
+	-- LSP
+	use("neovim/nvim-lspconfig") -- enable LSP
+	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
+	use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+
+	-- Telescope
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-media-files.nvim")
+	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("p00f/nvim-ts-rainbow")
+	use("nvim-treesitter/playground")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+
+	-- git
+	use("lewis6991/gitsigns.nvim")
+	use("APZelos/blamer.nvim")
+	use("kdheepak/lazygit.nvim")
+
+	use({ "tami5/lspsaga.nvim" }) -- nightly
+
+	-- Copilot
+	use("github/copilot.vim")
+
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
+end)
---
+++ lua/shaun/remaps.lua
@@ -0,0 +1,116 @@
+--Remap space as leader key
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+local keymap = vim.api.nvim_set_keymap
+local term_opts = { silent = true }
+local opt = { noremap = true }
+local opts = { noremap = true, silent = true }
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap('i', 'jj', '<Esc>', opt)
+
+-- keymap('n', '<c-i>', '<Esc>:so ~/.config/nvim/init.lua<CR>', opt)
+-- keymap('n', '<C-h>', ':bprev<CR>', opt)
+-- keymap('n', '<C-l>', ':bnext<CR>', opt)
+keymap('n', '<leader>c', ':bd<CR>', opt)
+keymap('n', '<leader>nh', ':noh<CR>', opt)
+keymap('n', 'Y', 'y$', opt)
+keymap('n', '<C-c>', ':tabclose<CR>', opt)
+keymap('n', 'H', ':tabprevious<CR>', opt)
+keymap('n', 'L', ':tabnext<CR>', opt)
+
+--- Center me naughty
+keymap('n', 'n', 'nzzzv', opt)
+keymap('n', 'N', 'Nzzzv', opt)
+keymap('n', 'J', "mzJ'z", opt)
+
+--- Sort the UNFORGIVABLE undo
+keymap('i', ',', ',<c-g>u', opt)
+keymap('i', '.', '.<c-g>u', opt)
+keymap('i', '!', '!<c-g>u', opt)
+keymap('i', '?', '?<c-g>u', opt)
+keymap('i', '<c-b>', 'mm<CR><ESC>`mi', opt)
+
+
+--- Move text
+keymap('v', 'J', ':m \'>+1<CR>gv=gv', opt)
+keymap('v', 'K', ':m \'<-2<CR>gv=gv', opt)
+keymap('i', '<C-j>', ':m .+1<CR>==', opt)
+keymap('i', '<C-j>', ':m .-2<CR>==', opt)
+keymap('n', '<leader>j', ':m .+1<CR>==', opt)
+keymap('n', '<leader>k', ':m .-2<CR>==', opt)
+
+
+
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+
+
+-- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<C-l>", "<cmd>lua require'telescope.builtin'.find_files()<cr>", opts)
+keymap("n", "<C-p>", "<cmd>lua require'telescope.builtin'.git_files()<cr>", opts)
+keymap("n", "<C-b>", "<cmd>lua require'telescope.builtin'.buffers()<cr>", opts)
+keymap("n", "<C-g>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<C-m>", ":NvimTreeToggle<cr>", opts)
+keymap("n", "<C-n>", ":NvimTreeFindFileToggle<cr>", opts)
+keymap("n", "<leader>f", ":Format<cr>", opts)
+
+keymap("n", "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<cr>", opts)
